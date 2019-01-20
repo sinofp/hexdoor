7fb3c42dbe20ed377060222eb8b85ba5712daae5
sinofp <achinese@live.cn>
2019-01-20 21:47:58 +0800

    第一次上传代码
    完成单色框架，字符仅区分utf8可打印字符和不可打印字符（以×表示）。

---
+++ hexdoor.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import fire
+
+HEADER = '┌────────┬─────────────────────────┬─────────────────────────┬────────┬────────┐'
+FOOTER = '└────────┴─────────────────────────┴─────────────────────────┴────────┴────────┘'
+LINE_FORMATTER = '│{:08x}│ {}┊ {}│{}│{}│'
+
+def hexhandle(file_name):
+    '''打印指定文件的16进制、可打印字符'''
+    print(HEADER)
+    with open(file_name, 'rb') as f_obj:
+        row = 0
+        line = f_obj.read(16)
+        while line:
+            line_hex = line.hex().ljust(32)
+            line_char = line.ljust(16)
+            print(LINE_FORMATTER.format(
+                row,
+                ''.join([line_hex[i:i+2]+' ' for i in range(0, 16, 2)]),
+                ''.join([line_hex[i:i+2]+' ' for i in range(16, 32, 2)]),
+                ''.join([chr(i) if chr(i).isprintable() else '×' for i in line_char[:8]]),
+                ''.join([chr(i) if chr(i).isprintable() else '×' for i in line_char[8:]])
+                ))
+            row += 0x10
+            line = f_obj.read(16)
+    print(FOOTER)
+
+if __name__ == '__main__':
+    fire.Fire(hexhandle)

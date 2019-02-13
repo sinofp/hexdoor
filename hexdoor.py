52f4ec4bd62c7410e57193829c1ec6ad5c66876b
sinofp <achinese@live.cn>
2019-02-13 20:11:02 +0800

    store color schemes in a list

--- hexdoor.py
+++ hexdoor.py
@@ -8,35 +8,41 @@ FOOTER = '└────────┴─────────────�
 LINE_FORMATTER = '│' + Style.DIM + '{:08x}' + Style.RESET_ALL + '│ {}' + Style.RESET_ALL + '│{}' + Style.RESET_ALL + '│'
 
 def hexstr2chr(s: str) -> chr:
+    return chr(int(s, 16))
+
+def hexstr2int(s: str) -> int:
     if '  ' == s:
-        # is padding
-        return '尾'
+        return 256
     else:
-        return chr(int(s, 16))
+        return int(s, 16)
+
+def stripandrjust(s: str) -> str:
+    return s[2:].rjust(2, '0')
 
 def colored(s: str) -> str:
     ch = hexstr2chr(s)
-    if '尾' == ch:
-        # pad the last line
-        return '  ', ' '
-    elif '\x00' == ch:
+    if '\x00' == ch:
         # NULL bytes
-        return Fore.RESET + Style.DIM + s + Style.RESET_ALL, Fore.RESET + Style.DIM + '0' + Style.RESET_ALL
+        return Fore.RESET + Style.DIM + stripandrjust(s) + Style.RESET_ALL, Fore.RESET + Style.DIM + '0' + Style.RESET_ALL
     elif ch in string.ascii_letters + string.digits + string.punctuation:
         # printable ASCII characters
-        return Fore.CYAN + s, Fore.CYAN + ch
+        return Fore.CYAN + stripandrjust(s), Fore.CYAN + ch
     elif ch in string.whitespace:
         # ASCII whitespace characters
-        return Fore.GREEN + s, ' ' if ' ' == ch else Fore.GREEN + '_'
+        return Fore.GREEN + stripandrjust(s), ' ' if ' ' == ch else Fore.GREEN + '_'
     elif int(s, 16) in range(128):
         # other ASCII characters
-        return Fore.MAGENTA + s, Fore.MAGENTA + '•'
+        return Fore.MAGENTA + stripandrjust(s), Fore.MAGENTA + '•'
     else:
         # non-ASCII characters
-        return Fore.YELLOW + s, Fore.YELLOW + '×'
+        return Fore.YELLOW + stripandrjust(s), Fore.YELLOW + '×'
 
 def hexhandle(file_name):
     '''print hex-string and ascii characters of given file'''
+    
+    cache = [colored(str(hex(b))) for b in range(256)] #0x00 - 0xff
+    cache.append(('  ', ' ')) # padding for the last line
+
     print(HEADER)
     with open(file_name, 'rb') as f_obj:
         row = 0
@@ -47,7 +53,7 @@ def hexhandle(file_name):
             hexbytes = ''
             printable = ''
             for i in range(0, len(line_hex), 2):
-                hbyte, abyte = colored(line_hex[i:i+2])
+                hbyte, abyte = cache[hexstr2int(line_hex[i:i+2])]
                 hexbytes += hbyte + ' ' if i != 14 else hbyte + Style.RESET_ALL + ' ┊ '
                 printable += abyte if i != 14 else abyte + Style.RESET_ALL + '┊'
 
